57f4cb35271aa7d4614f340cb853a0afac9c677a
tiggs <d@vidkim.com>
2015-07-28 01:38:55 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+
+var restify = require('restify');
+var server = restify.createServer();
+var TiggsBot = new require('./lib/tiggsbot'),
+    tiggsbot = new TiggsBot();
+var Twitch = new require('./lib/twitch'),
+    twitch = new Twitch();
+
+
+tiggsbot.connect();
+
+
+server.use(restify.queryParser());
+
+server.get('/', function (req, res, cb) {
+    if (!tiggsbot.isActive) {
+        console.log('connecting to irc!');
+        tiggsbot.connect();
+    }
+    var d = new Date();
+    res.send('hello, its currently ' + d);
+    return cb();
+});
+
+server.get('/disconnect', function (req, res, cb) {
+    tiggsbot.disconnect();
+    res.send(tiggsbot.isActive);
+    return cb();
+});
+
+server.get('/connect', function (req, res, cb) {
+    if (!tiggsbot.isActive) {
+        console.log('connecting to irc!');
+        tiggsbot.connect();
+    }
+    var d = new Date();
+    res.send('hello, its currently ' + d);
+    return cb();
+});
+
+server.get('/test', function (req, res, cb) {
+    res.send(tiggsbot.say());
+
+    return cb();
+});
+
+server.listen(process.env.PORT || 5000, function () { 
+    console.log('%s listening at %s', server.name, server.url);
+});
+
+server.get(/\/js|css|images\/?.*/, restify.serveStatic({
+    directory: './assets'
+}));
